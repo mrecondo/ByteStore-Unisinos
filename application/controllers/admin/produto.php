ff5be41a8c18297436c3bb676b792a5014b4b4f4
Michel Recondo <michel@recondo.com.br>
2011-08-20 14:11:24 -0300

    Ajustes na configuracao de autenticacao e exemplo em produtos

--- application/controllers/admin/produto.php
+++ application/controllers/admin/produto.php
@@ -8,6 +8,10 @@ class Produto extends CI_Controller {
         $this->load->model('produto_model');
         $this->load->library(array('table', 'pagination'));
         $this->load->helper('url');
+        
+        if (!$this->ion_auth->logged_in()) {
+            redirect('auth/login');
+        }
     }
 
     public function index() {
