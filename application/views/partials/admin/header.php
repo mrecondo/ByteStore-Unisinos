a2cbd6a5bae042e8b9d2e5b237336fce5346fbc7
Anderson Isotton <anisotton@hotmail.com>
2011-09-10 16:21:18 -0300

    atualizacoes

--- application/views/partials/admin/header.php
+++ application/views/partials/admin/header.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html>
     <head>
@@ -31,4 +32,37 @@
                 ?>
             </div>
         </div>
+=======
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title><?php echo $title; ?></title>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/css/admin/main.css" />
+        <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    </head>
+    <body>
+        <div id="header">
+            <h1><?php echo $title; ?></h1>
+            <div id="menu">
+               <?php     
+                echo anchor('administrator', "Home", 'class="itens_menu"');
+                echo "\n";
+                echo anchor('admin/cliente', "Clientes", 'class="itens_menu"');
+                echo "\n";
+                echo anchor('admin/produtos', 'Produtos', 'class="itens_menu"');
+                echo "\n";
+                echo anchor('admin/categoria', 'Categorias', 'class="itens_menu"');
+                echo "\n";
+                echo anchor('admin/fornecedor', 'Fornecedores', 'class="itens_menu"');
+                echo "\n";
+                echo anchor('admin/pedido', 'Pedidos', 'class="itens_menu"');
+                echo "\n";
+                echo anchor('auth', 'Usuarios', 'class="itens_menu"');
+                echo "\n";
+                echo anchor('auth/logout', 'Sair', 'class="itens_menu"');
+                ?>
+            </div>
+        </div>
+>>>>>>> branch 'master' of https://github.com/mrecondo/ByteStore-Unisinos.git
         <div id="conteudo" >
